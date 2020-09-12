b106f11c4d73280a353c16760a874bd0214d4ea0
Dominic Parry <Lethiathan2019@outlook.com>
2020-09-12 15:28:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+const express = require("express");
+const config = require('./config.js');
+const path = require("path");
+const fs = require("fs");
+const rateLimit = require("express-rate-limit");
+const app = express();
+
+app.enable("trust proxy");
+ 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+ 
+//  apply to all requests
+app.use(limiter);
+
+app.use('/', express.static('./public'));
+
+for (let page of config.pages) {
+	let root = './pages/';
+	if (fs.existsSync(`./pages/custom/${page}.html`)) {
+		root = './pages/custom/';
+	}
+	if (page === 'index') {
+		app.get('/', async (req, res, next) => await res.sendFile(`${page}.html`, { root: root }));
+	} else {
+		app.get(`/${page}`, async (req, res, next) => await res.sendFile(`${page}.html`, { root: root }));
+	}
+}
+
+
+const port = process.env.PORT || config.port;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
+});
